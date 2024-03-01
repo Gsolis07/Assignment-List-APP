63a50381ad059f5c77968aa624fb9330864763b5
Gsolis07 <gabriels1307@gmail.com>
2024-02-29 18:31:28 -0600

    update repo

--- assignment.py
+++ assignment.py
@@ -18,3 +18,52 @@ async def add_assignment(assignment: AssignmentRequest) -> dict:
     json_compatible_item_data = newAssignment.model_dump()
     return JSONResponse(json_compatible_item_data, status=status.HTTP_201_CREATED)
 
+@assignment_router.get("/todos")
+async def get_assignments() -> dict:
+    json_compatible_item_data = jsonable_encoder(assignment_list)
+    return JSONResponse(content=json_compatible_item_data)
+
+
+@assignment_router.get("/assignmentss/{id}")
+async def get_assignment_by_id(id: int = Path(..., title="default")) -> dict:
+    for assignment in assignment_list:
+        if assignment.id == id:
+            return {"assignment": assignment}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The assignment with ID={id} is not found.",
+    )
+        
+@assignment_router.get("/assignmentss/{id}")
+async def get_assignment_by_course(course: str = Path(..., title="default")) -> dict:
+    for assignment in assignment_list:
+        if assignment.course == course:
+            return {"assignment": assignment}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The assignment with course={course} is not found.",
+    )
+
+
+@assignment_router.put("/todos/{id}")
+async def update_assignment(assignment: AssignmentRequest, id: int) -> dict:
+    for x in assignment_list:
+        if x.id == id:
+            x.title = assignment.title
+            x.description = assignment.description
+            x.course = assignment.course
+            x.dueDate = assignment.dueDate
+            return {"message": "Assignment updated successfully"}
+
+    return {"message": f"The Assignment with ID={id} is not found."}
+
+
+@assignment_router.delete("/assignments/{id}")
+async def delete_assignment(id: int) -> dict:
+    for i in range(len(assignment_list)):
+        assignment = assignment_list[i]
+        if assignment.id == id:
+            assignment_list.pop(i)
+            return {"message": f"The Assignment with ID={id} has been deleted."}
+
+    return {"message": f"The Assignment with ID={id} is not found."}
---
+++ frontend/main.js
@@ -0,0 +1,157 @@
+let titleInput = document.getElementById('title');
+let descInput = document.getElementById('desc');
+let courseInput = document.getElementById('course');
+let dueDateInput = document.getElementById("dueDate")
+
+let assignmentId = document.getElementById('assignment-id');
+
+let titleEditInput = document.getElementById('title-edit');
+let descEditInput = document.getElementById('desc-edit');
+let courseEditInput = document.getElementById('course-edit');
+let dueDateEditInput = document.getElementById('dueDate-edit')
+
+let assignments = document.getElementById('assignments');
+let data = [];
+let selectedAssignment = {};
+
+const api = 'http://localhost:8000';
+
+function tryAdd() {
+    let msg = document.getElementById('msg');
+    msg.innerHTML = '';
+}
+
+document.getElementById('form-add').addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    if (!titleInput.value) {
+        document.getElementById('msg').innerHTML = 'Assignment cannot be blank';
+    } else {
+      addTodo(titleInput.value, courseInput.value, descInput.value, dueDateInput.value);
+  
+      // close modal
+      let add = document.getElementById('add');
+      add.setAttribute('data-bs-dismiss', 'modal');
+      add.click();
+      (() => {
+        add.setAttribute('data-bs-dismiss', '');
+      })();
+    }
+});
+
+let addTodo = (title, course, description, dueDate) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 201) {
+        const newAssignment = JSON.parse(xhr.responseText);
+        data.push(newAssignment);
+        refreshAssignments();
+      }
+    };
+    xhr.open('POST', `${api}/assignments`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify({ title, course, description, dueDate }));
+};
+
+let refreshTodos = () => {
+    assignments.innerHTML = '';
+    data
+      .sort((a, b) => b.id - a.id)
+      .map((x) => {
+        return (assignments.innerHTML += `
+        <div id="assignment-${x.id}" class="assignment-entry">
+          <div class="assignment-header">
+            <span class="fw-bold fs-4">${x.title}</span>
+            <span class="course">${x.course}</span>
+            <pre class="text-secondary ps-3">${x.description}</pre>
+            <span class="due-date">${x.dueDate}</span>
+          </div>
+        
+        <div class="options">
+          <i onClick="tryEditAssignment(${x.id})" data-bs-toggle="modal" data-bs-target="#modal-edit" class="fas fa-edit"></i>
+          <i onClick="deleteAssignment(${x.id})" class="fas fa-trash-alt"></i>
+        </div>
+      </div>
+    `);
+    });
+    resetForm();
+};
+
+let tryEditAssignment = (id) => {
+    const assignment = data.find((x) => x.id === id);
+    selectedAssignment = assignment;
+    assignmentId.innerText = assignment.id;
+    titleEditInput.value = assignment.title;
+    descEditInput.value = assignment.description;
+    courseEditInput.value = assignment.course;
+    dueDateEditInput.value = assignment.dueDate;
+    document.getElementById('msg').innerHTML = '';
+};
+
+document.getElementById('form-edit').addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    if (!titleEditInput.value) {
+      msg.innerHTML = 'Assignment cannot be blank';
+    } else {
+      editAssignment(titleEditInput.value, courseEditInput.value, descEditInput.value, dueDateEditInput.value);
+  
+      // close modal
+      let edit = document.getElementById('edit');
+      edit.setAttribute('data-bs-dismiss', 'modal');
+      edit.click();
+      (() => {
+        edit.setAttribute('data-bs-dismiss', '');
+      })();
+    }
+  });
+  let editAssignment = (title, course, description, dueDate) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        selectedAssignment.title = title;
+        selectedAssignment.course = course;
+        selectedAssignment.description = description;
+        selectedAssignment.dueDate = dueDate;
+        refreshAssignments();
+      }
+    };
+    xhr.open('PUT', `${api}/assignments/${selectedAssignment.id}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify({ title, course, description, dueDate }));
+  };
+  
+  let deleteAssignment = (id) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        data = data.filter((x) => x.id !== id);
+        refreshAssignments();
+      }
+    };
+    xhr.open('DELETE', `${api}/assignments/${id}`, true);
+    xhr.send();
+  };
+  
+  let resetForm = () => {
+    titleInput.value = '';
+    courseInput.value = '';
+    descInput.value = '';
+    dueDateInput.value = '';
+  };
+  
+  let getAssignments = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        data = JSON.parse(xhr.responseText) || [];
+        refreshAssignments();
+      }
+    };
+    xhr.open('GET', `${api}/assignments`, true);
+    xhr.send();
+  };
+  
+  (() => {
+    getAssignments();
+  })();
